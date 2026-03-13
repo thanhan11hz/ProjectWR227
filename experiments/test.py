93cc309f4b8d21415e5caf96f57abd9720fa686c
Nguyễn Thanh An <thanhan11hz@gmail.com>
2026-03-13 16:46:01 +0700

    Fix test and inference code

--- experiments/test.py
+++ experiments/test.py
@@ -1,38 +1,79 @@
 import sys
 from pathlib import Path
-
-ROOT = Path(__file__).resolve().parents[1]   # project_root
-sys.path.append(str(ROOT))
-
 import json
+import yaml
 from ultralytics import YOLO
 
+ROOT = Path(__file__).resolve().parents[1]
+
+sys.path.insert(0, str(ROOT / "ultralytics"))
+sys.path.insert(0, str(ROOT))
+
 WEIGHTS = ROOT / "outputs/baseline+se/weights/best.pt"
-DATASET = ROOT / "configs/dataset.yaml"
+
+DATA_ROOT = ROOT / "dataset"
+IMAGES_TEST = DATA_ROOT / "images/test"
+LABELS_TEST = DATA_ROOT / "labels/test"
+
+OUTPUT = ROOT / "outputs/metrics"
+OUTPUT.mkdir(parents=True, exist_ok=True)
+
+
+def create_dataset_yaml(img_dir, yaml_path):
+
+    data = {
+        "path": str(DATA_ROOT),
+        "train": "",
+        "val": "",
+        "test": str(img_dir.relative_to(DATA_ROOT)),
+        "names": {
+            0: "license_plate"
+        }
+    }
+
+    with open(yaml_path, "w") as f:
+        yaml.safe_dump(data, f)
 
 
 def evaluate():
 
     model = YOLO(WEIGHTS)
 
-    metrics = model.val(
-        data=DATASET,
-        split="test",
-        save_json=True
-    )
+    results_all = {}
 
-    results = {
-        "mAP50": metrics.box.map50,
-        "mAP50-95": metrics.box.map,
-        "mAP75": metrics.box.map75,
-        "precision": metrics.box.mp,
-        "recall": metrics.box.mr
-    }
+    subfolders = [f for f in IMAGES_TEST.iterdir() if f.is_dir()]
 
-    with open(ROOT / "outputs/metrics/results.json", "w") as f:
-        json.dump(results, f, indent=4)
+    for folder in subfolders:
 
-    print(results)
+        print(f"\nEvaluating corruption: {folder.name}")
+
+        yaml_path = ROOT / "configs/temp_test.yaml"
+
+        create_dataset_yaml(folder, yaml_path)
+
+        metrics = model.val(
+            data=yaml_path,
+            split="test",
+            save_json=True,
+            plots=False
+        )
+
+        result = {
+            "mAP50": float(metrics.box.map50),
+            "mAP75": float(metrics.box.map75),
+            "mAP50-95": float(metrics.box.map),
+            "precision": float(metrics.box.mp),
+            "recall": float(metrics.box.mr),
+            "fitness": float(metrics.fitness),
+            "per_class_mAP": [float(x) for x in metrics.box.maps]
+        }
+
+        results_all[folder.name] = result
+
+        print(result)
+
+    with open(OUTPUT / "results_by_corruption.json", "w") as f:
+        json.dump(results_all, f, indent=4)
 
 
 if __name__ == "__main__":
