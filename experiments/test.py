bb687361db6c1e9b6ec6476c54b91289db152671
Nguyễn Thanh An <thanhan11hz@gmail.com>
2026-05-11 10:50:15 +0700

    Train yolov5

--- experiments/test.py
+++ experiments/test.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT / "ultralytics"))
 sys.path.insert(0, str(ROOT))
 
-WEIGHTS = ROOT / "outputs/baseline/weights/best.pt"
+WEIGHTS = ROOT / "outputs/v5_baseline/weights/best.pt"
 
 DATA_ROOT = ROOT / "data/"
 IMAGES_TEST = DATA_ROOT / "images/test"
